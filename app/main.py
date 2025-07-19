933249fe47ccc0f3dc50609a2ef05727a8b86435
bcyong <1450115+bcyong@users.noreply.github.com>
2025-07-18 17:06:20 -0700

    Add keyterm router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from loguru import logger
-from .routers import health, audit, transcribe, webhook
+from .routers import health, audit, transcribe, webhook, keyterm
 
 logger.info("Starting the application")
 
@@ -10,3 +10,4 @@ app.include_router(health.router)
 app.include_router(audit.router)
 app.include_router(transcribe.router)
 app.include_router(webhook.router)
+app.include_router(keyterm.router)
---
+++ app/routers/keyterm.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter
+from ..utils import keyterm_manager
+import pydantic
+from loguru import logger
+
+router = APIRouter(prefix="/api/v1/keyterm")
+
+
+class KeytermRequest(pydantic.BaseModel):
+    keyterm: str
+
+
+@router.post("/add")
+async def add_keyterm(keyterm_request: KeytermRequest):
+    logger.info(f"Adding keyterm: {keyterm_request.keyterm}")
+    keyterm_manager.add_keyterm(keyterm_request.keyterm)
+    return {"message": "Keyterm added"}
+
+
+@router.post("/delete")
+async def delete_keyterm(keyterm_request: KeytermRequest):
+    logger.info(f"Deleting keyterm: {keyterm_request.keyterm}")
+    keyterm_manager.delete_keyterm(keyterm_request.keyterm)
+    return {"message": "Keyterm deleted"}
